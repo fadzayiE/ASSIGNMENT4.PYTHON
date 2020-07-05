74eefd36df3c9b59c544b7ec2b182393e11be898
fadzayiE <64942751+fadzayiE@users.noreply.github.com>
2020-07-05 04:45:42 -0400

    Add files via upload

---
+++ MapList.py
@@ -0,0 +1,6 @@
+list = ["is", "way", "three"]
+
+def listlength(list):
+ return list(map(lambda x: len(x), list))
+
+print ("list lengths in array => " + str(length(list)))
---
+++ Vawoles.py
@@ -0,0 +1,6 @@
+def is_Vowel(char):
+
+    if char == 'a' or 'e' or 'i' or 'o' or 'u' or 'A' or 'E' or 'I' or 'O' or 'U':
+        return True
+    else:
+        return False
